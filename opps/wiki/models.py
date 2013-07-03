e1bdf9d45b077e1266a8252da70f3b0449425bd7
Davi Oliveira <arlocm@gmail.com>
2013-07-03 14:14:40 -0300

    adding base wiki model

---
+++ opps/wiki/models.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from django.db.models import get_model
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+from taggit.models import TaggedItemBase
+from taggit.managers import TaggableManager
+
+from opps.core.models import NotUserPublishable, Slugged, Imaged
+
+
+class TaggedWiki(TaggedItemBase):
+    """Tag for wiki """
+    content_object = models.ForeignKey('wiki.WikiContent')
+
+
+class WikiContent(NotUserPublishable, Slugged):
+    title = models.CharField(_(u"title"), max_length=140)
+    tags = TaggableManager(
+        blank=True,
+        through=TaggedWiki,
+        verbose_name=u'Tags'
+    )
+    child_class = models.CharField(
+        _(u'child class'),
+        max_length=30,
+        db_index=True,
+        editable=False
+    )
+    child_app_label = models.CharField(
+        _(u'child app label'),
+        max_length=30,
+        db_index=True,
+        editable=False
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.child_class = self.__class__.__name__
+            self.child_app_label = self._meta.app_label
+        super(WikiContent, self).save(*args, **kwargs)
+
+    def get_child_model(self):
+        child_model = get_model(self.child_app_label, self.child_class)
+        if child_model == WikiContent:
+            return self
+
+        return child_model._default_manager.get(pk=self.pk)
+
+
+class Page(WikiContent):
+    content = models.TextField(_(u'content'))
