656a9a8dfff6f3c23ca8b4334a07d30b48329255
Luan Pablo <luanpab@gmail.com>
2013-07-10 15:21:07 -0300

    updating admin

--- opps/wiki/contrib/musicians/admin.py
+++ opps/wiki/contrib/musicians/admin.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
+
 from django.contrib import admin
 from django import forms
 
 from opps.wiki.admin import WikiAdmin
 
-from .models import Artist, Band
+from .models import Album, Band, Embed, Musician
 
 
-admin.site.register(Artist, WikiAdmin)
+admin.site.register(Album, WikiAdmin)
 admin.site.register(Band, WikiAdmin)
+admin.site.register(Embed, WikiAdmin)
+admin.site.register(Musician, WikiAdmin)
